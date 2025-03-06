a1cb5dccecb17f299dedd91c5827d35b68b39250
ark-key <76620843+ark-key@users.noreply.github.com>
2025-03-06 20:36:35 +0200

    add line breaks to increase readability in purchase.cy.ts

--- cypress/e2e/ui/purchase.cy.ts
+++ cypress/e2e/ui/purchase.cy.ts
@@ -16,14 +16,17 @@ describe('Purchase Flow', () => {
     cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click();
     cy.get('[data-test="shopping-cart-badge"]').should('contain', 1);
     cy.get('[data-test="shopping-cart-link"]').click();
+
     cy.url().should('include', '/cart.html');
     verifyCartItems();
     cy.get('[data-test="checkout"]').click();
+
     cy.url().should('include', '/checkout-step-one.html');
     cy.get('[data-test="firstName"]').type('John');
     cy.get('[data-test="lastName"]').type('Doe');
     cy.get('[data-test="postalCode"]').type('5000');
     cy.get('[data-test="continue"]').click();
+
     cy.url().should('include', '/checkout-step-two.html');
     verifyCartItems();
     cy.get('[data-test="payment-info-value"]').should(
@@ -36,6 +39,7 @@ describe('Purchase Flow', () => {
     );
     cy.get('[data-test="subtotal-label"]').should('contain', '29.99');
     cy.get('[data-test="finish"]').click();
+
     cy.url().should('include', '/checkout-complete.html');
     cy.get('[data-test="secondary-header"]').should(
       'contain',
